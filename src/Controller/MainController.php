185da7834339324d951bd6918a93a742cccf04f9
Skateside <sk85ide@hotmail.com>
2025-06-07 17:58:34 +0100

    Initial work backend scripting

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 // use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -19,25 +20,80 @@ class MainController extends AbstractController
 
     // TODO: Route for the sheet/summary
 
-    #[Route('/get-url', name: 'get_url')]
-    public function getUrlAction(
-        // #[MapRequestPayload(filter: \FILTER_VALIDATE_URL)] string $url,
-    ): JsonResponse
+    #[Route('/get-url', name: 'get_url', methods: ['POST'])]
+    public function getUrlAction(Request $request): JsonResponse
     {
-        // return $this->json(['success' => false, 'error' => 'get-url not set up']);
+        $url = $request->getPayload()->get('url');
+
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            return $this->jsonError('error.url_not_valid');
+        }
+
+        try {
+            $contents = file_get_contents($url);
+        } catch (\Exception $ignore) {
+            $contents = false;
+        }
+
+        if ($contents === false) {
+            return $this->jsonError('error.cannot_access');
+        }
+
+        $json = json_decode($contents);
+
+        if ($json === null) {
+            return $this->jsonError('error.invalid_json');
+        }
+
+        return $this->jsonSuccess($json);
+    }
+
+    #[Route('/get-botc', name: 'get_botc', methods: ['POST'])]
+    public function getBotcAction(Request $request): JsonResponse
+    {
+        $term = $request->getPayload()->get('term');
+
+        return $this->jsonError("get-botc not set up, but given '{$term}'");
+
+        // if (!filter_var($url, FILTER_VALIDATE_URL)) {
+        //     return $this->jsonError('error.url_not_valid');
+        // }
+
+        // try {
+        //     $contents = file_get_contents($url);
+        // } catch (\Exception $ignore) {
+        //     $contents = false;
+        // }
+
+        // if ($contents === false) {
+        //     return $this->jsonError('error.cannot_access');
+        // }
+
+        // $json = json_decode($contents);
+
+        // if ($json === null) {
+        //     return $this->jsonError('error.invalid_json');
+        // }
+
+        // return $this->jsonSuccess($json);
+    }
+
+    protected function jsonResponse(mixed $body, bool $success = true): JsonResponse
+    {
         return $this->json([
-            'success' => false,
-            'body' => 'get-url is not set up',
+            'success' => $success,
+            'body' => $body,
         ]);
     }
 
-    // #[Route('/get-botc', name: 'get_botc')]
-    // public function getBotcAction(
-    //     #[MapRequestPayload] string $botc,
-    // ): JsonResponse
-    // {
-    //     return $this->json(['success' => false, 'error' => 'get-botc not set up']);
-    // }
+    protected function jsonSuccess(mixed $body): JsonResponse
+    {
+        return $this->jsonResponse($body, true);
+    }
 
+    protected function jsonError(mixed $body): JsonResponse
+    {
+        return $this->jsonResponse($body, false);
+    }
 
 }
