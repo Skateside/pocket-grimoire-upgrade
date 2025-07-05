d589bc5b6d55e30bac3cf953447b58dcad6e2578
Skateside <sk85ide@hotmail.com>
2025-07-05 17:35:57 +0100

    Initial work on token modal

--- assets/components/Grimoire.vue
+++ assets/components/Grimoire.vue
@@ -1,22 +1,82 @@
 <template>
     <div ref="grimoire" class="grimoire movable">
 
-        <div
+        <button
             v-for="seat in store.byType.seat"
+            type="button"
             class="seat movable__item"
-            :data-id="seat.id"
+            :id="seat.id"
             :style='{
                 "--x": seat.x,
                 "--y": seat.y,
                 "--z": seat.z,
             }'
             :title="seat.id"
+            @movable-click="handleSeatClick"
         >
-            {{ seat.id.slice(0, 8) }}
-        </div>
+            {{ seat.id.slice(6, 12) }}
+        </button>
 
     </div>
-    
+
+    <div
+        ref="menu"
+        @toggle="handleMenuToggle"
+        popover
+    >
+        <p>{{ currentTokenId }}</p>
+
+        <p><strong>Role</strong></p>
+        <menu>
+            <li><button type="button">Set</button></li>
+            <li><button type="button">Show</button></li>
+            <li><button type="button">Shroud</button></li>
+            <li><button type="button">Ghost vote</button></li>
+            <li><button type="button">Rotate</button></li>
+        </menu>
+        <fieldset>
+            <legend>Set alignment</legend>
+            <!--
+            NOTE: This means different things for different role types:
+                Townsfolk & Outsiders = good, evil
+                Minions & Demon = evil, good
+                Travellers = unknown, good, evil
+                Fabled = unknown
+            Also: hide this is there's only 1 image.
+            -->
+            <ul>
+                <li>
+                    <label :for="`alignment-0-${idSuffix}`">
+                        <input type="radio" name="alignment" value="0" :id="`alignment-0-${idSuffix}`">
+                        Default
+                    </label>
+                </li>
+                <li>
+                    <label :for="`alignment-1-${idSuffix}`">
+                        <input type="radio" name="alignment" value="1" :id="`alignment-1-${idSuffix}`">
+                        Alternative
+                    </label>
+                </li>
+            </ul>
+        </fieldset>
+
+        <p><strong>Reminder</strong></p>
+        <menu>
+            <li><button type="button">Add</button></li>
+        </menu>
+        <p>TODO: Recent reminders</p>
+
+        <p><strong>Player</strong></p>
+        <menu>
+            <li><button type="button">Remove</button></li>
+        </menu>
+        <form>
+            <label :for="`name-${idSuffix}`">What is the name of this player?</label>
+            <input type="text" name="name" :id="`name-${idSuffix}`">
+            <button type="submit">Set player name</button>
+        </form>
+    </div>
+
     <p><button type="button" @click="addSeat">Add seat</button></p>
     <p>
         <label for="remove-dropdown">Remove</label>
@@ -34,10 +94,11 @@ import type {
     ICoordinates,
 } from "../scripts/types/data";
 import {
-    nextTick,
+    // computed,
     onMounted,
     onUnmounted,
     ref,
+    useId,
 } from "vue";
 import useTokenStore from "../scripts/store/token";
 import {
@@ -55,6 +116,8 @@ type IPad = {
     b: number,
 };
 
+const idSuffix = useId();
+const menu = ref<HTMLElement | null>(null);
 const store = useTokenStore();
 const grimoire = ref<HTMLElement | null>(null);
 const isDragging = ref<boolean>(false);
@@ -65,6 +128,8 @@ const pad = ref<IPad>({
     b: 0,
 });
 const removeDropdown = ref<HTMLSelectElement | null>(null);
+const currentTokenId = ref<string>("");
+// const currentToken = computed(() => store.getById(currentTokenId.value));
 
 const addSeat = () => {
 
@@ -85,38 +150,26 @@ const removeSeat = () => {
 };
 
 const getMovableItem = (target: Element) => {
-    return (target as HTMLElement).closest<HTMLElement>(".movable__item[data-id]");
+    return (target as HTMLElement).closest<HTMLElement>(".movable__item");
 };
 
 let dragHandler: (event: MouseEvent | TouchEvent) => void = noop;
 
-const getId = (movableItem: HTMLElement | null) => {
-
-    return (
-        movableItem?.hasAttribute("data-id")
-        ? movableItem.dataset.id
-        : ""
-    );
-
-};
-
 const moveTo = (movableItem: HTMLElement, { x, y, z }: ICoordinates) => {
 
-    const id = getId(movableItem);
+    const { id } = movableItem;
 
     if (!id) {
         return;
     }
 
-    if (typeof z !== "number" || Number.isNaN(z)) {
-        z = store.nextZ;
+    const update: ICoordinates = { x, y };
+
+    if (typeof z === "number" && !Number.isNaN(z)) {
+        update.z = z;
     }
 
-    store.update(id, {
-        x,
-        y,
-        z,
-    });
+    store.update(id, update);
 
 };
 
@@ -159,7 +212,7 @@ const dragObject = (moveableItem: HTMLElement, event: MouseEvent | TouchEvent) =
 const startDrag = (event: MouseEvent | TouchEvent) => {
 
     const movableItem = getMovableItem(event.target as HTMLElement);
-    const id = getId(movableItem);
+    const { id } = movableItem || {};
 
     if (!movableItem || !id) {
         return;
@@ -190,9 +243,7 @@ const endDragging = () => {
     // need to delay the resetting of `isDragging` so that the handler attached
     // to the click event doesn't trigger after dragging. This only seems to be
     // an issue on desktop - mobile seems to be fine.
-    nextTick(() => {
-        isDragging.value = false;
-    });
+    window.requestAnimationFrame(() => isDragging.value = false);
 
 };
 
@@ -204,7 +255,25 @@ const checkClick = (event: MouseEvent) => {
         return;
     }
 
-    // emit.trigger("movable-click", element);
+    element.dispatchEvent(new CustomEvent("movable-click", {
+        bubbles: true,
+        cancelable: true,
+    }));
+
+};
+
+const handleSeatClick = (event: Event) => {
+
+    currentTokenId.value = (event.target as HTMLElement).id || "";
+    menu.value?.showPopover();
+
+};
+
+const handleMenuToggle = (event: ToggleEvent) => {
+
+    if (event.newState === "closed") {
+        currentTokenId.value = "";
+    }
 
 };
 
@@ -289,8 +358,9 @@ onUnmounted(() => {
     position: absolute;
     left: calc(var(--x) * 100%);
     top: calc(var(--y) * 100%);
+    transform: translate(-50%, -50%);
     z-index: var(--z);
     touch-action: none;
-    transform: translate(-50%, -50%);
+    user-select: none;
 }
 </style>
