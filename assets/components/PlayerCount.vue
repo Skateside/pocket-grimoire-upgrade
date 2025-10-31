deeed281a826929314cfea1d798c393e479d8eed
James Long <sk85ide@hotmail.com>
2025-10-31 21:29:53 +0000

    Styling player count and tweaking role token leaves

--- assets/components/PlayerCount.vue
+++ assets/components/PlayerCount.vue
@@ -1,5 +1,5 @@
 <template>
-    <table>
+    <table class="count-table">
         <thead>
             <tr>
                 <th scope="row">Player Count</th>
@@ -8,13 +8,19 @@
                     :key="count"
                     scope="col"
                 >
-                    {{ count }}
+                    {{ count === "15" ? "15+" : count }}
                 </th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="(breakdown, team) in store.table">
-                <th scope="row">{{ team }}</th>
+            <tr
+                v-for="(breakdown, team) in store.table"
+                :class="[`count-row--${team}`, {
+                    'count-row--good': team === 'townsfolk' || team === 'outsider',
+                    'count-row--evil': team === 'minion' || team === 'demon',
+                }]"
+            >
+                <th scope="row">{{ labels[team] || team }}</th>
                 <td v-for="count in breakdown">{{ count }}</td>
             </tr>
         </tbody>
@@ -25,4 +31,20 @@
 import useGameStore from "../scripts/store/game";
 
 const store = useGameStore();
+const labels = {
+    townsfolk: "Townsfolk",
+    outsider: "Outsiders",
+    minion: "Minions",
+    demon: "Demons",
+};
 </script>
+
+<style lang="scss" scoped>
+.count-row--good {
+    color: var(--colour-good-team);
+}
+
+.count-row--evil {
+    color: var(--colour-evil-team);
+}
+</style>
