919f8c98245a81bc851e378cccbf80a5e55a3933
James Long <sk85ide@hotmail.com>
2025-11-22 22:20:54 +0000

    Allow player count to be changed

--- assets/components/PlayerCount.vue
+++ assets/components/PlayerCount.vue
@@ -25,17 +25,39 @@
                 <td
                     v-for="(count, key) in breakdown"
                     :key="key"
-                    :class="{ 'is-count': store.count === counts[key] - 5 }"
+                    :class="{ 'is-count': store.count === counts[key - 5] }"
                 >
                     {{ count }}
                 </td>
             </tr>
         </tbody>
     </table>
+
+    <!-- TODO: move this into its own component -->
+    <p>
+        <label :for="`player-count-${suffix}`">Player count</label>
+        <input
+            type="range"
+            :id="`player-count-${suffix}`"
+            min="5"
+            max="15"
+            step="1"
+            :value="store.count"
+            @input="setPlayerCount"
+            :list="`player-count-list-${suffix}`"
+        >
+        <datalist :id="`player-count-list-${suffix}`">
+            <option value="5" label="5"></option>
+            <option value="10" label="10"></option>
+            <option value="15" label="15"></option>
+        </datalist>
+        <output :value="store.count" aria-hidden="true"></output>
+    </p>
 </template>
 
 <script setup lang="ts">
 import useGameStore from "../scripts/store/game";
+import { useId } from "vue";
 
 const store = useGameStore();
 const labels = {
@@ -45,6 +67,11 @@ const labels = {
     demon: "Demons",
 };
 const counts = Object.keys(store.numbers).map(Number);
+
+const suffix = useId();
+const setPlayerCount = (event: Event) => {
+    store.setCount(Number((event.target as HTMLInputElement).value));
+};
 </script>
 
 <style lang="scss" scoped>
