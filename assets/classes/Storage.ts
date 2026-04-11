66524a92cfacce06614ead378ec1388ed9fc3b81
Skateside <sk85ide@hotmail.com>
2026-04-11 12:22:00 +0100

    Prevent object polution in storage

--- assets/classes/Storage.ts
+++ assets/classes/Storage.ts
@@ -1,7 +1,6 @@
-import type { AnyObject } from "../types/lib";
-import { isString } from "../utilities/objects";
+import type { AnyObject } from "~/types/lib";
+import { clone, isString } from "~/utilities/objects";
 
-//*
 export type IStorage = {
     set(key: string, value: any): void,
     get<TData>(key: string, validate: (raw: unknown) => boolean, defaultValue: TData): TData,
@@ -19,15 +18,14 @@ export default class Storage<TDefault extends AnyObject = AnyObject> implements 
 
         this.key = key;
         this.defaultValue = Object.create(null);
-        this.parsed = this.defaultValue;
+        this.parsed = clone(this.defaultValue);
 
         const stored = localStorage.getItem(key);
 
         if (Object.hasOwn(localStorage, key) && isString(stored)) {
 
             try {
-                const parsed = JSON.parse(stored);
-                this.parsed = Object.assign(Object.create(null), parsed);
+                this.parsed = JSON.parse(stored)
             } catch (ignore) {
                 // do nothing, `this.parsed` will remain its previous value.
             }
@@ -64,7 +62,7 @@ export default class Storage<TDefault extends AnyObject = AnyObject> implements 
     }
 
     reset() {
-        this.parsed = this.defaultValue;
+        this.parsed = clone(this.defaultValue);
         this.store();
     }
 
@@ -73,74 +71,3 @@ export default class Storage<TDefault extends AnyObject = AnyObject> implements 
     }
 
 }
-/*/
-export default class Storage implements IStorage {
-
-    protected key: string;
-    protected defaultValue: string;
-
-    constructor(key: string, defaultValue: AnyObject = {}) {
-
-        this.key = key;
-        this.defaultValue = JSON.stringify(defaultValue);
-
-    }
-
-    protected getStored(): AnyObject {
-        return JSON.parse(localStorage.getItem(this.key) || this.defaultValue);
-    }
-
-    protected store(stored: AnyObject) {
-        localStorage.setItem(this.key, JSON.stringify(stored));
-    }
-
-    has(key: string) {
-        return Object.hasOwn(this.getStored(), key);
-    }
-
-    get<T = any>(key: string, defaultValue?: T) {
-        return (this.getStored()[key] ?? defaultValue) as T;
-    }
-
-    set(key: string, value: any) {
-
-        const stored = this.getStored();
-
-        stored[key] = value;
-        this.store(stored);
-
-        return true;
-
-    }
-
-    remove(key: string) {
-
-        const stored = this.getStored();
-        const had = Object.hasOwn(stored, key);
-
-        delete stored[key];
-        this.store(stored);
-
-        return had;
-
-    }
-
-    reset() {
-        this.store(JSON.parse(this.defaultValue));
-    }
-
-    kill() {
-        localStorage.removeItem(this.key);
-    }
-
-}
-
-export type IStorage = {
-    has(key: string): boolean,
-    get<T = any>(key: string, defaultValue?: T): T,
-    set(key: string, value: any): boolean,
-    remove(key: string): boolean,
-    reset(): void,
-    kill(): void,
-};
-//*/
