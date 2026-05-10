d28e02012cf81e6e7e599e4e86b0fb3727f52bbd
Skateside <sk85ide@hotmail.com>
2026-05-10 16:10:25 +0100

    Add preset filters to the role select modal

--- assets/views/grimoire/GrimoireWrapper.vue
+++ assets/views/grimoire/GrimoireWrapper.vue
@@ -3,7 +3,7 @@
 
     <GridLayout>
         <div>
-            <BaseButton @click="showRoles">Add token</BaseButton>
+            <BaseButton @click="() => showRoles('basic')">Add token</BaseButton>
             <!-- should be able to add traveller/fabled/loric -->
         </div>
         <div>
@@ -24,7 +24,11 @@
         @set-role="() => roleSelectModal?.show()"
     />
 
-    <RoleSelectModal ref="role-select-modal" @role-click="setSeatRole" />
+    <RoleSelectModal
+        ref="role-select-modal"
+        :preset="roleSelectModalPreset"
+        @role-click="setSeatRole"
+    />
 
     <BasePopup ref="popup" />
 </template>
@@ -36,6 +40,7 @@ import useTokensStore from "~/stores/tokens";
 import GrimoirePad from "./GrimoirePad.vue";
 import BasePopup from "~/components/base/BasePopup.vue";
 import BaseButton from "~/components/base/BaseButton.vue";
+import type { IRoleSelectModalPreset } from "./RoleSelectModal.vue";
 import RoleSelectModal from "./RoleSelectModal.vue";
 import SeatMenuModal from "./SeatMenuModal.vue";
 import GridLayout from "~/components/layouts/GridLayout.vue";
@@ -43,6 +48,7 @@ import GridLayout from "~/components/layouts/GridLayout.vue";
 const tokensStore = useTokensStore();
 const seatMenuModal = useTemplateRef("seat-menu-modal");
 const roleSelectModal = useTemplateRef("role-select-modal");
+const roleSelectModalPreset = ref<IRoleSelectModalPreset>(null);
 const popup = useTemplateRef("popup");
 const theTokenId = ref<ITokenSeat["id"] | undefined>(undefined);
 
@@ -81,7 +87,8 @@ const setSeatRole = (roleId: IRole["id"]) => {
 
 };
 
-const showRoles = () => {
+const showRoles = (preset: IRoleSelectModalPreset = null) => {
+    roleSelectModalPreset.value = preset;
     roleSelectModal.value?.show();
 };
 </script>
--- assets/views/grimoire/RoleSelectModal.vue
+++ assets/views/grimoire/RoleSelectModal.vue
@@ -1,10 +1,10 @@
 <template>
     <BaseModal ref="modal" title="Select Role">
-        <details>
+        <details v-if="showFilters">
             <summary><h2>Filter</h2></summary>
             <StackLayout>
-                <h3>Teams</h3>
-                <ClusterLayout>
+                <h3 v-if="showTeamsFilter">Teams</h3>
+                <ClusterLayout v-if="showTeamsFilter">
                     <!-- TODO: i18n -->
                     <BaseLabel
                         v-for="team in ORDER"
@@ -18,8 +18,8 @@
                         />
                     </BaseLabel>
                 </ClusterLayout>
-                <h3>Exclude</h3>
-                <StackLayout>
+                <h3 v-if="showExcludeFilter">Exclude</h3>
+                <StackLayout v-if="showExcludeFilter">
                     <BaseLabel label="Exclude roles in-play" :nested="true">
                         <BaseCheckbox
                             v-model="excludeRoles.inPlay"
@@ -36,7 +36,7 @@
             </StackLayout>
         </details>
 
-        <h2>Results</h2>
+        <h2 v-if="showFilters">Results</h2>
 
         <GridLayout min-width="6ch">
             <button
@@ -51,10 +51,13 @@
     </BaseModal>
 </template>
 
+<script lang="ts">
+export type IRoleSelectModalPreset = null | "basic" | "travellers" | "special";
+</script>
 <script setup lang="ts">
 import type { IRole, IRoleFilter } from "~/types/data";
 import { ERoleTeam } from "~/enums/data";
-import { computed, reactive, useTemplateRef } from "vue";
+import { computed, onMounted, reactive, ref, useTemplateRef } from "vue";
 import BaseModal from "~/components/modal/BaseModal.vue";
 import BaseLabel from "~/components/base/BaseLabel.vue";
 import BaseCheckbox from "~/components/base/BaseCheckbox.vue";
@@ -66,12 +69,18 @@ import useRolesStore from "~/stores/roles";
 import useTokensStore from "~/stores/tokens";
 import { ORDER } from "~/helpers/roles";
 
+const props = defineProps<{
+    preset?: IRoleSelectModalPreset,
+}>();
 const emit = defineEmits<{
     (e: "role-click", roleId: IRole["id"]): void,
 }>();
 const rolesStore = useRolesStore();
 const tokensStore = useTokensStore();
 const modal = useTemplateRef("modal");
+const showTeamsFilter = ref(true);
+const showExcludeFilter = ref(true);
+const showFilters = computed(() => showTeamsFilter.value || showExcludeFilter.value);
 
 const teams = reactive<Record<ERoleTeam, boolean>>({
     [ERoleTeam.TOWNSFOLK]: true,
@@ -108,6 +117,54 @@ const filters = computed(() => {
         include,
         exclude,
     };
+});
+
+onMounted(() => {
+
+    const showTeams: ERoleTeam[] = [];
+
+    switch (props.preset) {
+        case "basic": {
+            showTeamsFilter.value = false;
+            showExcludeFilter.value = false;
+            showTeams.push(...[
+                ERoleTeam.TOWNSFOLK,
+                ERoleTeam.OUTSIDER,
+                ERoleTeam.MINION,
+                ERoleTeam.DEMON,
+            ]);
+            excludeRoles.inPlay = false;
+            excludeRoles.notOnScript = true;
+            break;
+        }
+        case "travellers": {
+            showTeamsFilter.value = false;
+            showTeams.push(...[
+                ERoleTeam.TRAVELLER,
+            ]);
+            excludeRoles.inPlay = false;
+            excludeRoles.notOnScript = false;
+            break;
+        }
+        case "special": {
+            showTeamsFilter.value = false;
+            showExcludeFilter.value = false;
+            showTeams.push(...[
+                ERoleTeam.FABLED,
+                ERoleTeam.LORIC,
+            ]);
+            excludeRoles.inPlay = false;
+            excludeRoles.notOnScript = true;
+            break;
+        }
+    }
+
+    if (showTeams.length) {
+        Object.keys(teams).forEach((team) => {
+            teams[team as ERoleTeam] = showTeams.includes(team as ERoleTeam);
+        });
+    }
+
 });
 
 defineExpose({
